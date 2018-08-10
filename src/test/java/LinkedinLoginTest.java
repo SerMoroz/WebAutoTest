91156377696baafff262fed7e77c96556393be0d
serhii.moroz.ext <ok.mailbox777@gmail.com>
2018-08-10 19:20:39 +0300

    Lesson4 Home task

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,31 +3,63 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import static java.lang.Thread.sleep;
+
 
 public class LinkedinLoginTest {
+    WebDriver browser;
+    private boolean profileNavigationItemDisplayed;
 
+    @BeforeMethod
+    public void beforeMethod() {
+        browser = new FirefoxDriver();
+        browser.get("https://www.linkedin.com/");
+    }
 
-    @Test
-    public void successfulLoginTest () {
-
+    @AfterMethod
+    public void afterMethod() {
+        browser.close();
     }
 
     @Test
-    public void negativeLoginTest () {
-        WebDriver browser = new FirefoxDriver();
-        browser.get ("https://www.linkedin.com/");
-        WebElement userEmailField = browser.findElement(By.xpath("//*[@id=\"login-email\"]"));
-        WebElement userPasswordField = browser.findElement(By.xpath("//*[@id=\"login-password\"]"));
-        WebElement signInButton = browser.findElement(By.xpath("//*[@id=\"login-submit\"]"));
+    public void successfulLoginTest() throws InterruptedException {
+
+        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(browser);
+        linkedinLoginPage.login("XXX", "XXX");
+
+        LinkedinHomePage linkedinHomePage = new LinkedinHomePage(browser);
+        linkedinHomePage.isProfileNavigationItemDisplayed();
+
+        sleep(3000);
+        String pageTitle = browser.getTitle();
+        Assert.assertEquals(pageTitle, "LinkedIn", "Home page title is wrong.");
+
+        String checkURL = browser.getCurrentUrl();
+        Assert.assertEquals(checkURL, "https://www.linkedin.com/feed/", "Home page URL is wrong.");
+
+        Assert.assertTrue( linkedinHomePage.isProfileNavigationItemDisplayed(), "'profileNavigationItem' is not displayed on Home page");
 
 
+    }
+
+
+    @Test
+    public void negativeLoginTest() {
+        WebElement userEmailField = browser.findElement(By.xpath("//*[@id='login-email']"));
+        WebElement userPasswordField = browser.findElement(By.xpath("//*[@id='login-password']"));
+        WebElement signInButton = browser.findElement(By.xpath("//*[@id='login-submit']"));
         userEmailField.sendKeys("a@b.c");
         userPasswordField.sendKeys("wrong");
         signInButton.click();
+
         WebElement alertBox = browser.findElement(By.xpath("//*[@role='alert']"));
         Assert.assertEquals(alertBox.getText(), "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.", "Alert box has incorrect message");
 
-
     }
+
+
 }
+
